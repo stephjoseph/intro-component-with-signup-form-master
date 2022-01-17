5901b7b67da4042a663ef3a42adce884702c7d00
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-01-17 15:29:28 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    extend: {
+      boxShadow: {
+        inner: "inset 0 -4px 0 0 rgb(0 0 0 / 0.0909)",
+      },
+    },
+  },
+  plugins: [],
+};
